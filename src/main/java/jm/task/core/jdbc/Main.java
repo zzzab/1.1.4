b7b05c9913e37b15d46d661999f19f9cbbf62d57
Zabiya Mustafaeva <mustafaevazabiya@gmail.com>
2024-12-05 20:21:57 +0500

    sout added

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -16,7 +16,7 @@ public class Main {
         userService.saveUser("Name3", "LastName3", (byte) 31);
         userService.saveUser("Name4", "LastName4", (byte) 38);
 
-        System.out.println(userService.getAllUsers().toString());
+        System.out.println(userService.getAllUsers());
         userService.removeUserById(1);
         System.out.println(userService.getAllUsers());
         userService.cleanUsersTable();
