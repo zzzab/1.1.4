c869fa8d842a8384b3659db85362efb4b5c4fd45
Zabiya Mustafaeva <mustafaevazabiya@gmail.com>
2024-12-05 22:20:15 +0500

    class main edited

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -8,16 +8,11 @@ public class Main {
     public static void main(String[] args) {
         Util.getConnection();
         UserService userService = new UserServiceImpl();
-
         userService.createUsersTable();
-
         userService.saveUser("Name1", "LastName1", (byte) 20);
         userService.saveUser("Name2", "LastName2", (byte) 25);
         userService.saveUser("Name3", "LastName3", (byte) 31);
         userService.saveUser("Name4", "LastName4", (byte) 38);
-
-        System.out.println(userService.getAllUsers());
-        userService.removeUserById(1);
         System.out.println(userService.getAllUsers());
         userService.cleanUsersTable();
         userService.dropUsersTable();
